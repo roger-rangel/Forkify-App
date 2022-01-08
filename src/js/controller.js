00b5ef3ab9f21663eb2bedc543af6f0fc3ada5f8
Roger Rangel <roger.rangel.ee@gmail.com>
2022-01-08 18:46:38 +0100

    Reimplement Search Results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js'
+import resultsView from './views/resultsView.js'
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +30,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+    
     // 1) Get search query
     const query = searchView.getQuery()
     if (!query) return;
@@ -34,6 +41,7 @@ const controlSearchResults = async function () {
     
     // 3) Render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results)
   } catch (error) {
     console.log(error);
   }
