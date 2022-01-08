5664ba62ba40bf144b877cec5ab7199f235f40f1
Roger Rangel <roger.rangel.ee@gmail.com>
2022-01-08 10:29:35 +0100

    Implement Search Results

--- src/js/model.js
+++ src/js/model.js
@@ -4,29 +4,52 @@ import { getJSON } from './helpers.js';
 
 export const state = {
     recipe: {},
+    search: {
+        query: '',
+        results: [],
+    }
 };
 
 export const loadRecipe = async function (id) {
     try {
         const data = await getJSON(`${API_URL}/${id}`)
         
-        console.log(data);
-        
         const { recipe } = data.data;
         state.recipe = {
-        id: recipe.id,
-        title: recipe.title,
-        publisher: recipe.publisher,
-        sourceURL: recipe.source_url,
-        image: recipe.image_url,
-        servings: recipe.servings,
-        cookingTime: recipe.cooking_time,
-        ingredients: recipe.ingredients
+            id: recipe.id,
+            title: recipe.title,
+            publisher: recipe.publisher,
+            sourceURL: recipe.source_url,
+            image: recipe.image_url,
+            servings: recipe.servings,
+            cookingTime: recipe.cooking_time,
+            ingredients: recipe.ingredients
         }
         
-        console.log(state.recipe);
+    } catch (error) {
+        console.error(`${error} 👻`)
+        throw error;
+    }
+};
+
+export const loadSearchResults = async function (query) {
+    try {
+        state.search.query = query;
+        
+        const data = await getJSON(`${API_URL}?search=${query}`);
+        
+        state.search.results = data.data.recipes.map(rec => {
+            return {
+                id: rec.id,
+                title: rec.title,
+                publisher: rec.publisher,
+                image: rec.image_url,
+            }
+        })
+        
     } catch (error) {
         console.error(`${error} 👻`)
         throw error;
     }
 }
+
